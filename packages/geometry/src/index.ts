bc4e9b221297260c8d26152be135dc993b6c29d8
Timon Orawski <timon@orawski.com>
2026-05-27 21:27:14 -0400

    feat(geometry): add Vec2 and CrossSection types

--- packages/geometry/src/index.ts
+++ packages/geometry/src/index.ts
@@ -1,2 +1,4 @@
 export { emptyMesh, vertexCount, triangleCount, computeBBox } from './mesh';
 export type { Vec3, Mesh, BBox } from './mesh';
+export { emptyCrossSection } from './cross-section';
+export type { Vec2, CrossSection } from './cross-section';
